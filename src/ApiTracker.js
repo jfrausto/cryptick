56c19e86731e6a20a54c91e5c74fc2ce7751d7cb
JOCECODE <ulah57@gmail.com>
2021-04-25 19:01:26 -0700

    CREATE BRANCH;

---
+++ src/ApiTracker.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ApiTracker() {
+ return (
+  <>
+    <h1>
+     
+     Hello There
+
+    </h1>
+    
+  </>
+ )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   theme,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import ApiTracker from "./ApiTracker";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -18,19 +18,7 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+         <ApiTracker/>
         </VStack>
       </Grid>
     </Box>
