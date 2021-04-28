72eb0602ce76d3b0931a776222b52b01e624c44a
jessef <1319957+jfrausto@users.noreply.github.com>
2021-04-28 12:06:32 -0700

    display BTC ticker price in view

--- src/components/TickerDisplay.tsx
+++ src/components/TickerDisplay.tsx
@@ -1,48 +1,45 @@
-import React, {useState, useRef, useEffect, MutableRefObject} from 'react';
-// import { NullLiteral } from 'typescript';
+import React, {useState, useRef, useEffect} from 'react';
 import { Heading, Button, Text } from '@chakra-ui/react';
 
+// interface TickerDisplayProps {
+//   // some properties
+// };
 
+// prop types <any> for now
 export const TickerDisplay:React.FC<any> = () => {
 
-  // const [price, setPrice] = useState("0.00");
-
-  type ws = React.MutableRefObject<null> | React.MutableRefObject<WebSocket>;
-  let webSocket: ws = useRef(null);
+  const [price, setPrice] = useState<number>(0.00);
+  const webSocket = useRef<null | WebSocket >(null);
 
   // const url = "https://api.pro.coinbase.com";
 
-
   // executes on first load of this component
   useEffect(() => {
-    // effect
-    // create a connection to websocket
+    // create a connection to websocket current ref
     webSocket.current = new WebSocket("wss://ws-feed.pro.coinbase.com");
-
+    console.log("I'm in use effect");
     return () => {
-      // cleanup
+      // cleanup/close ws
+      webSocket.current!.close();
     }
   }, []);
 
   // handle sub click
   const handleSub = () => {
-
     let msg = {
       type: "subscribe",
       product_ids: ["BTC-USD"],
       channels: ["ticker"]
     };
-
     let jsonMsg = JSON.stringify(msg);
     // send subscribe message
-    webSocket.current!.send(jsonMsg);
-
+    webSocket.current?.send(jsonMsg);
     // event listener that executes everytime we get a message from the socket
     webSocket.current!.onmessage = (e) => {
       let data = JSON.parse(e.data);
-      console.log(data);
+      console.log(data.price);
+      setPrice(data.price);
     };
-
   };
   
   // handle unsub click
@@ -57,14 +54,14 @@ export const TickerDisplay:React.FC<any> = () => {
     webSocket.current!.send(jsonUnsub);
   }
 
-
   return (
     <div>
       <Heading>
         BTC-USD Ticker
       </Heading>
       <Text fontStyle="italic">
-        BTC price goes here
+        {/* this actual price display area could be a separate component */}
+        {price ? price : "subscribe to watch!"}
       </Text>
       <Button m={3} onClick={handleSub}>
         subscribe
