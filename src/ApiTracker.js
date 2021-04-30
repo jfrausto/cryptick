ad890d1e176c544d6d0969f1d6228e545131d9c3
JOCECODE <ulah57@gmail.com>
2021-04-29 20:38:09 -0700

    CREATE API CALL ONE TIME IN CONSOLE AND PAGE;

--- src/ApiTracker.js
+++ src/ApiTracker.js
@@ -5,15 +5,24 @@ import {useEffect, useState} from 'react';
 export default function ApiTracker() {
 
 const [apiData, setApiData] = useState([]);
+let i = 0;
 
 useEffect(() => {
-  const fetchTasks = async () => {
-    const res = await fetch("https://api.pro.coinbase.com/products");
-    const data = await res.json();
-    console.log(data);
-  }
-  fetchTasks()
+ const getApiData = async () => {
+   const cryptoProductsList = await fetchCyrptoProducts();
+   setApiData(cryptoProductsList);
+ }
+
+ getApiData();
+
 }, []);
+
+const fetchCyrptoProducts = async () => {
+  const res = await fetch("https://api.pro.coinbase.com/products");
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
 
  return (
   <>
@@ -22,7 +31,17 @@ useEffect(() => {
      Hello There
 
     </h1>
-    <div></div>
+    <div>
+      <p>
+        Below are the api data list
+      </p>
+      {apiData.map((data) => (
+        <div key={i++}>
+        <h3 >{data.id}<span style={{color: "red"}}>  {data.quote_currency} </span></h3>
+        
+        </div>
+      ))}
+    </div>
   </>
  )
 
