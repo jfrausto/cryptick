093af153ea59b47cbb8e617a91022eaf6ac9ff9e
JOCECODE <ulah57@gmail.com>
2021-05-12 00:47:57 -0700

    CREATE COMPONET FOR NEXT; REFACTOR CODE;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import {
-  Text,
   VStack,
   Heading,
   
---
+++ src/pages/landingPage.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+
+
+export default function ApiTracker() {
+
+
+const [apiData, setApiData] = useState([]);
+
+// Used solely to make unique Key for the children props for apiData.map() line 40
+let i = 0;
+
+useEffect(() => {
+
+  const getApiData = async () => {
+   const cryptoProductsList = await fetchCyrptoProducts();
+   setApiData(cryptoProductsList);
+   console.log(cryptoProductsList);
+ }
+
+ getApiData();
+
+//Occurs on load
+}, []);
+
+// Should fetch the product information about the cryptocurrency
+const fetchCyrptoProducts = async () => {
+  const res = await fetch("https://api.pro.coinbase.com/products");
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+ return (
+  <>
+    <h1>
+     
+     Hello There
+
+    </h1>
+    <div>
+      <p>
+        Below are the api data list
+      </p>
+      <br/>
+      
+      {/* First filter data for cyrptocurrencies for USD then map the children and assigning unique key using i++ */}
+      {apiData.filter(data => data.quote_currency === "USD").map((data) => (
+        <div key={i++}>
+        
+        <h3>{data.id}<span style={{color: "red", fontSize: "13px"}}>  {data.quote_currency} </span></h3>
+        
+        </div>
+      ))}
+    </div>
+  </>
+ )
+
+}
