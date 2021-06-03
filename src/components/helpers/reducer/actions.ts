dfd078bd8f68342d321a60043e94152b236ef72e
jessef <1319957+jfrausto@users.noreply.github.com>
2021-06-02 19:26:25 -0700

    add Action type to CryptoAction interface

--- src/components/helpers/reducer/actions.ts
+++ src/components/helpers/reducer/actions.ts
@@ -4,7 +4,7 @@ export const SET_CURRENT_PAIR: Action = "set_current_pair";
 export const SET_PRICE: Action = "set_price";
 export const ARROW_UP: Action = "arrow_up"
 export interface CryptoAction {
-  type: "set_all_chosen_pairs" | "arrow_up" | "set_current_pair" | "set_price",
+  type: "set_all_chosen_pairs" | "arrow_up" | "set_current_pair" | "set_price" | Action,
   payload?: string | string[] | boolean,
   price?: number,
   percentageChange?: number
